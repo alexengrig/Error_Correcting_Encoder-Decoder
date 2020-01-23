86ca6d14559e9b6fae415ce600bbd4df464bbb7d
Grig Alex <alexengrigdev@gmail.com>
2020-01-23 21:55:03 +0300

    Add stage 2/5 solution

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -24,15 +24,45 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String word = scanner.nextLine();
-        char[] chars = word.toCharArray();
+        String message = scanner.nextLine();
+        System.out.println(message);
+        char[] chars = message.toCharArray();
         StringBuilder builder = new StringBuilder();
+        for (char ch : chars) {
+            builder.append(String.valueOf(ch).repeat(3));
+        }
+        String encodedMessage = builder.toString();
+        System.out.println(encodedMessage);
+        chars = encodedMessage.toCharArray();
+        builder = new StringBuilder();
         for (int i = 0; i < chars.length; i++) {
-            char ch = chars[i];
-            if (i % 3 == 0) {
-                builder.append(getRandom());
-            } else {
-                builder.append(ch);
+            int index = RANDOM.nextInt(3);
+            for (int j = 0; j < 3; j++, i++) {
+                char ch = chars[i];
+                if (j != index) {
+                    builder.append(ch);
+                } else {
+                    builder.append(getRandom());
+                }
+            }
+            --i;
+        }
+        String errorMessage = builder.toString();
+        System.out.println(errorMessage);
+        chars = errorMessage.toCharArray();
+        builder = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            char[] triple = new char[3];
+            for (int j = 0; j < 3; j++, i++) {
+                triple[j] = chars[i];
+            }
+            --i;
+            if (triple[0] == triple[1]) {
+                builder.append(triple[0]);
+            } else if (triple[1] == triple[2]) {
+                builder.append(triple[1]);
+            } else if (triple[2] == triple[0]) {
+                builder.append(triple[2]);
             }
         }
         System.out.println(builder);
