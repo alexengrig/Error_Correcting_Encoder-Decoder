a770d5f1f2ba7586ae8b82a22100a61dcaaa9473
Grig Alex <alexengrigdev@gmail.com>
2020-01-24 23:51:28 +0300

    Add stage 3/5 solution

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -1,74 +1,22 @@
 package correcter;
 
+import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    private static final String ALPHABET;
     private static final Random RANDOM = new Random();
 
-    static {
-        StringBuilder builder = new StringBuilder();
-        for (char i = 'A'; i <= 'Z'; i++) {
-            builder.append(i);
-        }
-        for (char i = 'a'; i <= 'z'; i++) {
-            builder.append(i);
-        }
-        for (char i = '0'; i <= '9'; i++) {
-            builder.append(i);
-        }
-        builder.append(' ');
-        ALPHABET = builder.toString();
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String message = scanner.nextLine();
-        System.out.println(message);
-        char[] chars = message.toCharArray();
-        StringBuilder builder = new StringBuilder();
-        for (char ch : chars) {
-            builder.append(String.valueOf(ch).repeat(3));
-        }
-        String encodedMessage = builder.toString();
-        System.out.println(encodedMessage);
-        chars = encodedMessage.toCharArray();
-        builder = new StringBuilder();
-        for (int i = 0; i < chars.length; i++) {
-            int index = RANDOM.nextInt(3);
-            for (int j = 0; j < 3; j++, i++) {
-                char ch = chars[i];
-                if (j != index) {
-                    builder.append(ch);
-                } else {
-                    builder.append(getRandom());
-                }
+        try (Scanner scanner = new Scanner(new FileInputStream(new File("send.txt")));
+             PrintWriter printer = new PrintWriter(new FileOutputStream(new File("received.txt")))) {
+            String message = scanner.nextLine();
+            char[] chars = message.toCharArray();
+            for (int ch : chars) {
+                printer.print((char) (ch ^ (1 << RANDOM.nextInt(7))));
             }
-            --i;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        String errorMessage = builder.toString();
-        System.out.println(errorMessage);
-        chars = errorMessage.toCharArray();
-        builder = new StringBuilder();
-        for (int i = 0; i < chars.length; i++) {
-            char[] triple = new char[3];
-            for (int j = 0; j < 3; j++, i++) {
-                triple[j] = chars[i];
-            }
-            --i;
-            if (triple[0] == triple[1]) {
-                builder.append(triple[0]);
-            } else if (triple[1] == triple[2]) {
-                builder.append(triple[1]);
-            } else if (triple[2] == triple[0]) {
-                builder.append(triple[2]);
-            }
-        }
-        System.out.println(builder);
-    }
-
-    private static char getRandom() {
-        return ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length()));
     }
 }
